242ee5f7e0a45545b9cf38b6e8a56535ff50e30d
Giovanni Peron <peron.giovanni@gmail.com>
2019-12-11 19:58:38 +0100

    google maps e geolocalizzazione aggiunti

--- trains/lib/screens/home.dart
+++ trains/lib/screens/home.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:trains/services/auth.dart';
 import 'package:trains/screens/destination.dart';
-class Home extends StatelessWidget {
 
-  final AuthService _authService = AuthService();
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:trains/services/geolocation.dart';
+
+import 'package:geolocator/geolocator.dart';
+
+
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+
+}
+
+class _HomeState extends State<Home> {
+  final _geolocationService = Geolocation();
+  Position _position;
+
+  _HomeState() {
+    _geolocationService.getCurrentPosition().then((val) => setState(() {
+      _position = val;
+    }));
+  }
+
+  //----- per vedere mappa
+  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  void _onMapCreated(GoogleMapController controller){
+    _controller.complete(controller);
+  }
+  //-----
+
 
   @override
   Widget build(BuildContext context) {
@@ -14,21 +43,70 @@ class Home extends StatelessWidget {
         backgroundColor: Color(0xff9b0014),
         elevation: 0.0,
       ),
-    body: Center(
-      child: RaisedButton (
-        color: Color(0xff9b0014),
-        child: Text(
-        'Cerca destinazione',
-        style: TextStyle(color: Colors.white),
-        ),
-        onPressed: () async {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Destination()),
-          );
-        },
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width, // or use fixed size like 200
+              height: 200,
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                myLocationEnabled: true,
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 11.0,
+                ),
+              )
+            ),
+            Text(
+              _position.toString(),
+            ),
+            RaisedButton(
+              color: Color(0xff9b0014),
+              child: Text(
+                'Cerca destinazione',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Destination()),
+                );
+              },
+            ),
+          ],
       ),
-    ),
+      ),
     );
   }
+
 }
+
+/*
+Center(
+RaisedButton(
+                color: Color(0xff9b0014),
+                child: Text(
+                  'Cerca destinazione',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Destination()),
+                  );
+                },
+              ),
+             ),
+              */
+/*
+GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: const LatLng(_position.latitude, _position.longitude),
+                  zoom: 11.0,
+                ),
+              ),
+      );
+ */
---
+++ trains/lib/services/geolocation.dart
@@ -0,0 +1,7 @@
+import 'package:geolocator/geolocator.dart';
+
+class Geolocation{
+  Future<Position> getCurrentPosition() async {
+    return await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  }
+}
