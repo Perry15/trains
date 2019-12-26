f61c1372600b723d868e9d4470c5afaac9da2af8
Matteo Marchiori <matteo.marchiori97@gmail.com>
2019-12-26 12:16:01 +0100

    tutorial jit ok, forse da migliorare

--- trains/lib/main.dart
+++ trains/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:trains/screens/valutazione_treno.dart';
 
 void main() {
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Container(child: ValutazioneTreno("ab123")));
+    return MaterialApp(
+        home: Container(
+            child: ValutazioneTreno("ab123", SharedPreferences.getInstance())));
   }
 }
--- trains/lib/screens/table_valutazione.dart
+++ trains/lib/screens/table_valutazione.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:showcaseview/showcaseview.dart';
-
 import 'package:trains/screens/valutatore.dart';
 
 class TableValutazione extends StatefulWidget {
+  final bool _tutorial;
+
+  TableValutazione(this._tutorial);
+
   @override
   _TableValutazioneState createState() => _TableValutazioneState();
 }
@@ -17,9 +20,11 @@ class _TableValutazioneState extends State<TableValutazione> {
   @override
   void initState() {
     super.initState();
-    //Start showcase view after current widget frames are drawn.
-    WidgetsBinding.instance.addPostFrameCallback((_) =>
-        ShowCaseWidget.of(context).startShowCase([_one, _two, _three, _four]));
+    if (widget._tutorial) {
+      WidgetsBinding.instance.addPostFrameCallback((_) =>
+          ShowCaseWidget.of(context)
+              .startShowCase([_one, _two, _three, _four]));
+    }
   }
 
   @override
@@ -30,20 +35,18 @@ class _TableValutazioneState extends State<TableValutazione> {
         TableRow(children: [
           SizedBox.shrink(),
           TableCell(
-            child: Showcase(
-              key: _one,
-              description: 'Trascinalo al centro se il treno è vuoto...',
-              child: Valutatore("Vuoto", Colors.green),
-            ),
+            child: _getTutorial('Vuoto', Colors.green, _one,
+                'Trascinalo al centro se il treno è vuoto...'),
           ),
           SizedBox.shrink(),
         ]),
         TableRow(children: [
           TableCell(
-            child: Showcase(
-              key: _two,
-              description: '...trascina questo se ci sono posti liberi...',
-              child: Valutatore("Quasi vuoto", Colors.yellow),
+            child: _getTutorial(
+              "Quasi vuoto",
+              Colors.yellow,
+              _two,
+              '...trascina questo se ci sono posti liberi...',
             ),
           ),
           TableCell(
@@ -60,23 +63,37 @@ class _TableValutazioneState extends State<TableValutazione> {
             print(data.toString());
           })),
           TableCell(
-              child: Showcase(
-            key: _three,
-            description: '...questo se il treno è affollato...',
-            child: Valutatore("Quasi pieno", Colors.orange),
+              child: _getTutorial(
+            "Quasi pieno",
+            Colors.orange,
+            _three,
+            '...questo se il treno è affollato...',
           ))
         ]),
         TableRow(children: [
           SizedBox.shrink(),
           TableCell(
-              child: Showcase(
-            key: _four,
-            description: '...o questo se non ci si può sedere.',
-            child: Valutatore("Pieno", Colors.red),
+              child: _getTutorial(
+            "Pieno",
+            Colors.red,
+            _four,
+            '...o questo se non ci si può sedere.',
           )),
           SizedBox.shrink(),
         ])
       ],
     );
+  }
+
+  Widget _getTutorial(
+      String value, MaterialColor color, GlobalKey key, String description) {
+    if (widget._tutorial) {
+      return Showcase(
+        key: key,
+        description: description,
+        child: Valutatore(value, color),
+      );
+    }
+    return Valutatore(value, color);
   }
 }
